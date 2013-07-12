975ffa677c4f908d87c5ac26f4eee68df643d0ee
Danny Greg <danny@github.com>
2013-07-11 17:16:30 -0700

    Stub in and sketch RBLHoverDecorationTextView.

---
+++ Rebel/RBLHoverDecorationTextView.h
@@ -0,0 +1,31 @@
+//
+//  RBLHoverDecorationTextView.h
+//  Rebel
+//
+//  Created by Danny Greg on 11/07/2013.
+//  Copyright (c) 2013 GitHub. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+// We've all found ourselves needing to change the attributes of text on
+// mouse-over, but that is laughably hard in stock AppKit.
+//
+// This subclass allows you to specify attributes which will be used at a given
+// range whenever the mouse hovers above.
+//
+// Caveats
+// -------
+// This currently only works with uneditable text views.
+@interface RBLHoverDecorationTextView : NSTextView
+
+// Sets the attributes which will be applied to the text at the given range.
+//
+// attributes - The attributes to be applied. See NSAttributedString.h for
+//              compatible keys.
+// range      - The range of text which the attributes should be applied to.
+//              Note that the attributes will be applied when the mouse hovers
+//              over this range, not any part of the text view.
+- (void)setHoverAttributes:(NSDictionary *)attributes atRange:(NSRange)range;
+
+@end
---
+++ Rebel/RBLHoverDecorationTextView.m
@@ -0,0 +1,28 @@
+//
+//  RBLHoverDecorationTextView.m
+//  Rebel
+//
+//  Created by Danny Greg on 11/07/2013.
+//  Copyright (c) 2013 GitHub. All rights reserved.
+//
+
+#import "RBLHoverDecorationTextView.h"
+
+@implementation RBLHoverDecorationTextView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
